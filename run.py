482601fd8f5971799a91abe7775b2452770c8a40
bizzybits <ponce.emc@gmail.com>
2022-07-14 12:25:41 -0700

    update db2 var

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ app = Flask(__name__)
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL"
+    "DATABASE_URL2"
 )  # "sqlite:///quizgame.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
