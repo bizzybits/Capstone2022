e5afc726998ac3d3304606f85379618b99be5dd5
bizzybits <ponce.emc@gmail.com>
2022-07-19 22:41:31 -0700

    adding flask mail module

--- models.py
+++ models.py
@@ -3,31 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Keys(db.Model):
     quiz_key = db.Column(db.Integer, primary_key=True)
 
+
 class Quiz(db.Model):
-    __tablename__ = 'quizzes'
+    __tablename__ = "quizzes"
     id = db.Column(db.Integer, primary_key=True)
-    candidate_id = db.Column(db.Integer, db.ForeignKey('candidates.id'))
-   # completed = db.Column(db.Boolean, server_default=False)
+    candidate_id = db.Column(db.Integer, db.ForeignKey("candidates.id"))
+    # completed = db.Column(db.Boolean, server_default=False)
     # Quizzes HAS MANY questions.
     # Candidate connection
     candidate = db.relationship("CandidateModel")
 
+
 # this is a table to relate two models Quiz with its Questions
 class QuizQuestions(db.Model):
-    __tablename__ = 'quiz_questions'
+    __tablename__ = "quiz_questions"
 
     id = db.Column(db.Integer, primary_key=True)
     quiz_id = db.Column(db.Integer, nullable=False)
     question_id = db.Column(db.Integer, nullable=False)
 
+
 class QuestionModel(db.Model):
     __tablename__ = "question_table"
 
     id = db.Column(db.Integer, primary_key=True)
-    question_id = db.Column(db.Integer(), unique=True)# no need? just use id above
+    question_id = db.Column(db.Integer(), unique=True)  # no need? just use id above
     question_label = db.Column(db.String())
     question_text = db.Column(db.String())
     answer = db.Column(db.String())
@@ -37,7 +41,16 @@ class QuestionModel(db.Model):
 
     # Questions BELONG TO MANY quizzes
 
-    def __init__(self, question_id, question_label, question_text, answer, options1, options2, options3):
+    def __init__(
+        self,
+        question_id,
+        question_label,
+        question_text,
+        answer,
+        options1,
+        options2,
+        options3,
+    ):
         self.question_id = question_id
         self.question_label = question_label
         self.question_text = question_text
@@ -54,14 +67,20 @@ class QuestionModel(db.Model):
 # TODO: create Candidate model.
 # TODO: create CandidateAnswers model.
 
-class Answer(db.Model):
-    __tablename__ = 'answers'
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    question_id = db.Column(db.Integer(), db.ForeignKey('question_table.id', ondelete="CASCADE"), nullable=False)
-    content = db.Column(db.String(), nullable=False)
-    correct = db.Column(db.Boolean(), server_default='FALSE', nullable=False)
 
-    question = db.relationship('QuestionModel')
+class Answer(db.Model):
+    __tablename__ = "answers"
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    question_id = db.Column(
+        db.Integer(),
+        db.ForeignKey("question_table.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    content = db.Column(db.String(), nullable=False)
+    correct = db.Column(db.Boolean(), server_default="FALSE", nullable=False)
+
+    question = db.relationship("QuestionModel")
+
 
 class AnswerModel(db.Model):
     __tablename__ = "answer_table"
